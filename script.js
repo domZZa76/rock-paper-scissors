ae8e769ef96555fb1b2ab43f929a063bdebadd62
Domagoj <domagojkatic1511@gmail.com>
2022-10-21 21:53:25 +0200

    make playRound func

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-//COMPUTER SELECTION 
-var computerChoice = 
+                        //COMPUTER SELECTION 
 
-Math.ceil(Math.random() * 3);
-console.log()
+let randNumber = 0;
+randNumber = Math.ceil(Math.random() * 3);
+
+function computerChoice(randNumber) {
+    if(randNumber === 1) {
+        return 'rock'
+    } else if (randNumber === 2) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+                        //PLAYER CHOICE
+
+let popUp = prompt('What do you choose?');
+let playerChoice = popUp.toLocaleLowerCase();
+
+                        //PLAY ROUND
+
+function playRound(playerChoice, computerChoice){
+    if (
+        playerChoice === computerChoice
+    ) {
+        return 'DRAW'
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        return 'WIN'
+    } else {
+        return 'LOSS'
+    }
+}
