00a69b62363b242f2464d4b78293eb7570751abe
Domagoj <domagojkatic1511@gmail.com>
2022-10-23 11:59:30 +0200

    console veryija gotova i sitne promijene

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-                        //COMPUTER SELECTION 
+//COMPUTER SELECTION 
 
 let randNumber = 0;
 randNumber = Math.ceil(Math.random() * 3);
 
-function computerChoice(randNumber) {
-    if(randNumber === 1) {
+function computerChoiceFormula(randNumber) {
+    if (randNumber === 1) {
         return 'rock'
     } else if (randNumber === 2) {
         return 'paper'
@@ -12,25 +12,37 @@ function computerChoice(randNumber) {
         return 'scissors'
     }
 }
-                        //PLAYER CHOICE
+//PLAY ROUND
 
-let popUp = prompt('What do you choose?');
-let playerChoice = popUp.toLocaleLowerCase();
-
-                        //PLAY ROUND
-
-function playRound(playerChoice, computerChoice){
+function playRound(playerChoice, computerChoice) {
     if (
         playerChoice === computerChoice
     ) {
-        return 'DRAW'
+        return tie
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
-        return 'WIN'
+        playerScore++
+        return win
     } else {
-        return 'LOSS'
+        computerScore++
+        return lose
     }
 }
+//PLAY GAME
+
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+let win = "You win"
+let lose = "You lose"
+let tie = "It is a tie"
+
+for(var i=0;i<5;i++){
+    let playerChoice = prompt("Pick a move");
+    const computerChoice = computerChoiceFormula(randNumber)
+    console.log(playRound(playerChoice, computerChoice))
+    console.log("your score = " + playerScore);
+    console.log("Computer's score = " + computerScore);
+  }
